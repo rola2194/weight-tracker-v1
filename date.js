7f69cc5545e6bfc001179dd40f9d3afa47e175c7
rola2194 <rola2194@gmail.com>
2025-04-04 17:07:11 +0200

    Delete all content from gh-pages

--- date.js
+++
@@ -1,8 +0,0 @@
-
-
-const dateEl = document.querySelector('.date-btn');
-
-const today = new Date();
-console.log(today);
-
-dateEl.textContent = today.toLocaleDateString('en-Uk', { year: 'numeric', month: 'short', day: '2-digit' });
